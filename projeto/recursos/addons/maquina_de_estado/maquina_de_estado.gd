07da01d281ea516003b9bce084f3a5204b91228b
hiroto <rmoraisu@protonmail.com>
2024-04-30 13:29:41 -0500

    comentarios atualizados #3

--- projeto/recursos/addons/maquina_de_estado/maquina_de_estado.gd
+++ projeto/recursos/addons/maquina_de_estado/maquina_de_estado.gd
@@ -16,13 +16,12 @@ onready var estado: Estado = get_node(estado_inicial)
 
 func _ready() -> void:
 	yield(owner, "ready")
-	# The state machine assigns itself to the State objects' state_machine property.
 	for crianca in get_children():
 		crianca.maquina_de_estado = self
 	estado.entrar()
 
 
-# The state machine subscribes to node callbacks and delegates them to the state objects.
+# Chamados do nódulo são delegados para o estado atual.
 func _unhandled_input(evento: InputEvent) -> void:
 	estado.manipular_entrada(evento)
 
@@ -35,15 +34,9 @@ func _physics_process(delta: float) -> void:
 	estado.atualizar_fisica(delta)
 
 
-# This function calls the current state's exit() function, then changes the active state,
-# and calls its enter function.
-# It optionally takes a `msg` dictionary to pass to the next state's enter() function.
+# Chama `sair` do estado atual, muda o estado atual, chama `entrar` do novo estado atual (usando `params`), emite `transicionado`
 func transicionar(novo_estado: String, params: Dictionary = {}) -> void:
-	# Safety check, you could use an assert() here to report an error if the state name is incorrect.
-	# We don't use an assert here to help with code reuse. If you reuse a state in different state machines
-	# but you don't want them all, they won't be able to transition to states that aren't in the scene tree.
-	if not has_node(novo_estado):
-		return
+	assert(not has_node(novo_estado), "Estado não econtrado: %s." % novo_estado)
 
 	estado.sair()
 	estado = get_node(novo_estado)
