b0244bf84005d3e99692d03b9923b92a70249448
ClockWritesDev <herbert@grupozenir.com.br>
2024-06-06 17:25:25 -0300

    mudanças

--- projeto/addons/Controle_de_Swipe/plugin.gd
+++ projeto/addons/Controle_de_Swipe/plugin.gd
@@ -2,7 +2,7 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("ControleDeSwipe", "Node", preload("res://addons/Controle_de_Swipe/controle_toque.gd"), load("icon.svg"))
+	add_custom_type("ControleDeSwipe", "Node", preload("res://addons/Controle_de_Swipe/controle_toque.gd"), preload("res://addons/Controle_de_Swipe/icon.svg"))
 
 
 func _exit_tree():
